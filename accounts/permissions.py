79487a85b7a39c6f3df2c50028d18870514a9bdc
kweku-xvi <nksarps@gmail.com>
2024-01-20 10:35:54 +0000

    added permission to check if user has verified their account

---
+++ accounts/permissions.py
@@ -0,0 +1,6 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsVerified(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_verified and request.user.is_authenticated
