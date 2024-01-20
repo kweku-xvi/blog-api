1b06def596af0636e8407c03ff59677af4166e4c
kweku-xvi <nksarps@gmail.com>
2024-01-20 10:35:26 +0000

    added users model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,53 @@
+import uuid
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
-# Create your models here.
+
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,password:None,**extra_fields):
+        if not email:
+            raise ValueError(_("The email is Required"))
+        email = self.normalize_email(email=email)
+        user = self.model(email=email,**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+
+    def create_superuser(self,email,password=None,**extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("SuperUser must have is_staff=True"))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("SuperUser must have is_superuser=True"))
+        
+        return self.create_user(email,password,**extra_fields)
+
+
+class User(AbstractUser):
+    id = models.CharField(max_length=8, primary_key=True, unique=True)
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, blank=True)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_verified = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.id = str(uuid.uuid4())[:8]
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ('-created_at',)
