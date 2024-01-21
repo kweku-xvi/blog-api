1050200b15cd04bc0eaa6074f3b083c4dac66230
kweku-xvi <nksarps@gmail.com>
2024-01-21 08:05:02 +0000

    added blogs to admin panel

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,10 @@
+from .models import Blog
 from django.contrib import admin
 
-# Register your models here.
+# Showing auto_now_add fields date_published and time_published in admin panel
+class BlogAdmin(admin.ModelAdmin): 
+    list_display = ('id', 'title', 'author', 'date_published')
+    readonly_fields = ('date_published', 'time_published')
+
+
+admin.site.register(Blog, BlogAdmin)
