837cb96e34da07feedf342074e7149018ce465e6
kweku-xvi <nksarps@gmail.com>
2024-01-23 11:12:33 +0000

    refactor admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,7 +1,7 @@
 from .models import Blog
 from django.contrib import admin
 
-# Showing auto_now_add fields date_published and time_published in admin panel
+
 class BlogAdmin(admin.ModelAdmin): 
     list_display = ('id', 'title', 'author', 'date_published')
     readonly_fields = ('date_published', 'time_published')
