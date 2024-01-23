29bf67ec77187d9391a1d048b8a8df23fd6ef6dc
kweku-xvi <nksarps@gmail.com>
2024-01-23 10:29:17 +0000

    refactor urls

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,9 +5,9 @@ from django.urls import path
 urlpatterns = [
     path('', views.get_all_blogs_view, name='get_all_blogs'),
     path('create', views.create_blog_view, name='created_blog'),
-    path('<str:blog_id>', views.get_specific_blog_view, name='get_blog'),
     path('user/<str:uid>', views.get_all_user_blogs_view, name='get_all_user_blogs'),
     path('<str:blog_id>/update', views.update_blog_view, name='update_blog'),
     path('<str:blog_id>/delete', views.delete_blog_view, name='delete_blog'),
     path('within-last-3-months', views.get_blogs_within_last_3_months_view, name='blogs_within_last_3_months'),
+    path('<str:blog_id>', views.get_specific_blog_view, name='get_blog'),
 ]
