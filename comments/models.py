782223dd8c7ebe327661279429f94325367fb285
kweku-xvi <nksarps@gmail.com>
2024-01-24 09:57:54 +0000

    added comments model

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,18 @@
+import uuid
+from accounts.models import User
+from blogs.models import Blog
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
+    comment = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.comment
+
+    class Meta:
+        ordering = ('-created_at',)
