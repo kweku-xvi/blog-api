d603b5163464dadd7a4a54634e46e900b293514c
kweku-xvi <nksarps@gmail.com>
2024-01-24 09:57:02 +0000

    added url endpoints for documentation

--- core/urls.py
+++ core/urls.py
@@ -15,10 +15,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v0.0.1',
+      description="A Blog API written with Django Rest Framework",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('blog/', include('blogs.urls')),
     path('comment/', include('comments.urls')),
+
+    path('swagger/json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
