ca6ca95f47888396adf41979d9e825f0e93254ee
kweku-xvi <nksarps@gmail.com>
2024-01-24 09:57:36 +0000

    added comments to admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,10 @@
+from .models import Comment
 from django.contrib import admin
 
-# Register your models here.
+
+class CommentAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+    list_display = ('id', 'blog', 'user', 'created_at')
+
+
+admin.site.register(Comment, CommentAdmin)
