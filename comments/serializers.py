4d0ea477eee375f63b4f02f6c119a3269989b66f
kweku-xvi <nksarps@gmail.com>
2024-01-24 09:58:08 +0000

    added comments serializer

---
+++ comments/serializers.py
@@ -0,0 +1,17 @@
+from .models import Comment
+from rest_framework import serializers
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    blog = serializers.SerializerMethodField()
+    user = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'comment', 'blog', 'user']
+
+    def get_user(self, obj):
+        return obj.user.username if obj.user else None
+
+    def get_blog(self, obj):
+        return obj.blog.title if obj.blog else None
